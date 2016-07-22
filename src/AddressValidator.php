013c1f10aae681f0d8d0471211f2150d887b092c
Simon Westlake <simon@sonar.software>
2016-07-21 21:11:38 -0500

    Don't output into src dir

--- src/AddressValidator.php
+++ src/AddressValidator.php
@@ -47,7 +47,7 @@ class AddressValidator
     public function validate($pathToImportFile)
     {
         if (($handle = fopen($pathToImportFile, "r")) !== FALSE) {
-            $tempFile = tempnam(__DIR__,"validatedAddresses");
+            $tempFile = tempnam(__DIR__ . "/../","validatedAddresses");
             $tempHandle = fopen($tempFile, "w");
 
             $this->validateImportFile($pathToImportFile);
