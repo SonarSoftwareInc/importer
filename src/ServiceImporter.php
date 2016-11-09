7c183e5be7be3e8103ad1fa0f0905ee6842a12c9
Simon Westlake <simon@sonar.software>
2016-11-09 12:33:13 -0600

    Implement import function for services

--- src/ServiceImporter.php
+++ src/ServiceImporter.php
@@ -3,7 +3,10 @@
 namespace SonarSoftware\Importer;
 
 use Exception;
+use GuzzleHttp\Client;
 use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Pool;
+use GuzzleHttp\Psr7\Request;
 use InvalidArgumentException;
 use SonarSoftware\Importer\Extenders\AccessesSonar;
 
@@ -31,6 +34,60 @@ class ServiceImporter extends AccessesSonar
                 'failure_log_name' => $failureLogName,
                 'success_log_name' => $successLogName,
             ];
+
+            $validData = [];
+            while (($data = fgetcsv($handle, 8096, ",")) !== FALSE) {
+                array_push($validData, $data);
+            }
+
+            $requests = function () use ($validData)
+            {
+                foreach ($validData as $validDatum)
+                {
+                    yield new Request("POST", $this->uri . "/api/v1/services/", [
+                            'Content-Type' => 'application/json; charset=UTF8',
+                            'timeout' => 30,
+                            'Authorization' => 'Basic ' . base64_encode($this->username . ':' . $this->password),
+                        ]
+                        , json_encode($this->buildPayload($validDatum)));
+                }
+            };
+
+            $client = new Client();
+
+            $pool = new Pool($client, $requests(), [
+                'concurrency' => 10,
+                'fulfilled' => function ($response, $index) use (&$returnData, $successLog, $failureLog, $validData)
+                {
+                    $statusCode = $response->getStatusCode();
+                    if ($statusCode > 201)
+                    {
+                        $body = json_decode($response->getBody()->getContents());
+                        $line = $validData[$index];
+                        array_push($line,$body);
+                        fputcsv($failureLog,$line);
+                        $returnData['failures'] += 1;
+                    }
+                    else
+                    {
+                        $returnData['successes'] += 1;
+                        fwrite($successLog,"Import succeeded for service {$validData[$index][1]}" . "\n");
+                    }
+                },
+                'rejected' => function($reason, $index) use (&$returnData, $failureLog, $validData)
+                {
+                    $response = $reason->getResponse();
+                    $body = json_decode($response->getBody()->getContents());
+                    $returnMessage = implode(", ",(array)$body->error->message);
+                    $line = $validData[$index];
+                    array_push($line,$returnMessage);
+                    fputcsv($failureLog,$line);
+                    $returnData['failures'] += 1;
+                }
+            ]);
+
+            $promise = $pool->promise();
+            $promise->wait();
 
         }
         else
