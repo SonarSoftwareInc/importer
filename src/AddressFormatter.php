365e872acc7b070072ae69929766977add738d15
Simon Westlake <simon@sonar.software>
2018-07-19 09:06:00 -0500

    Check for existence in the key too, need this for US/CA

--- src/AddressFormatter.php
+++ src/AddressFormatter.php
@@ -123,7 +123,7 @@ class AddressFormatter extends AccessesSonar
             $this->subDivisions[$unformattedAddress['country']] = array_map(function($value) { return strtolower($value); }, (array)$subDivisionObject->data);
         }
 
-        if (!in_array(trim(strtolower($unformattedAddress['state'])),$this->subDivisions[$unformattedAddress['country']]))
+        if (!in_array(trim(strtolower($unformattedAddress['state'])),$this->subDivisions[$unformattedAddress['country']]) && !isset($this->subDivisions[$unformattedAddress['country']]))
         {
             throw new InvalidArgumentException($unformattedAddress['state'] . " is not a valid subdivision for " . $unformattedAddress['country']);
         }
